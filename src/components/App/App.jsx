d1f06f4d10aa07637e5cc37de205e03872c09a2b
mayukh-B <rudrabagchi11@gmail.com>
2021-05-26 19:20:59 +0530

    cards added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import ResultsContainer from '../ResultsContainer/ResultsContainer'
 import SearchBox from '../SearchBox/SearchBox'
 import Header from './../Header/Header'
 import './App.css'
+
+const name = require('@rstacruz/startup-name-generator')
 
 class App extends React.Component {
     state = {
         headerText: 'Name It',
         isExpanded: true,
+        suggestedNames: [],
     }
 
     handleInputChange = (inputText) => {
-        this.setState({ isExpanded: !inputText })
+        this.setState({
+            isExpanded: !inputText,
+            suggestedNames: name(inputText),
+        })
     }
 
     render() {
@@ -21,6 +28,7 @@ class App extends React.Component {
                     headTitle={this.state.headerText}
                 />
                 <SearchBox onInputChange={this.handleInputChange} />
+                <ResultsContainer suggestedNames={this.state.suggestedNames} />
             </div>
         )
     }
